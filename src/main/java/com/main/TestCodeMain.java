932a7ad7e3b469e40e566c36bc1021eb62914dfe
Prashant Shukla <prashantshukla.its@gmail.com>
2020-02-17 11:00:07 +0530

    Added main class

---
+++ src/main/java/com/main/TestCodeMain.java
@@ -0,0 +1,14 @@
+package com.main;
+
+public class TestCodeMain {
+
+	public static void main(String[] args) {
+		System.out.println(getPlaceHolderString());
+
+	}
+
+	private static String getPlaceHolderString(){
+		return "Test Project Placeholder";
+	}
+
+}
